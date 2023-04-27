f17a7c98446629428fc4c37c2110e09fabc835f1
krchandler27 <krchandler27@gmail.com>
2023-04-26 20:44:56 -0700

    starter files for my portfolio

---
+++ src/components/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function About() {
+  return (
+
+    <section class="about-me">
+        <img class="katie-picture" src="./assets/images/KatiePic.jpg" alt="Picture of Katie Speakman" />
+        <article>
+            <h2 id="about-me-word"> About Me </h2>
+            <p class="info"> Hello! My name is Katie Speakman and I am a Charleston, South Carolina native. These days,
+                I find myself in the Wild West of Arizona. I recently graduated from the University of Arizona's
+                Full-Stack Coding Bootcamp. While a student there, I was able to design over 20 projects, three of which, I
+                worked on with a team. I'm glad you came across my portfolio. I would love to connect with you and build you the application of your dreams. You can click <a href="#contact-me">here</a> to get in touch with me. You can also click on the <a
+                    href="#my-portfolio">links</a> below to see some of my work. I look
+                forward to connecting with you.</p>
+            <p>-Katie</p>
+        </article>
+    </section>
+  );
+}
